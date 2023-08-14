64266e8bc16d701ba93ac26fd3adbbd8f28b8167
Dav1ik <111747295+David-Ivanov@users.noreply.github.com>
2023-08-14 17:21:29 +0300

    fix all

--- navigation.js
+++ navigation.js
@@ -5,7 +5,6 @@ const link = document.querySelectorAll(".nav__link");
 
 
 const openBtn = document.querySelector(".hotbar__btn");
-const closeBtn = document.querySelector(".nav__btn");
 const openDesktopBtn = document.querySelector(".hotbar__btn-desktop");
 
 
@@ -20,11 +19,6 @@ openDesktopBtn.addEventListener("click", function () {
     document.body.style.overflow = "hidden";
 });
 
-closeBtn.addEventListener("click", function () {
-    nav.classList.toggle("nav--active");
-    backface.classList.toggle("nav__backface--active");
-    document.body.style.overflow = "scroll";
-});
 
 link.forEach((value) => {
     value.addEventListener("click", function () {
